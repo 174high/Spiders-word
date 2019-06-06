61a7104bc577d90006355df21e58cf66ef875b7a
Johnny SHI <johnny.shi@schindler.com>
2019-06-06 16:54:02 +0800

    add alignment of work generation

--- change_paragraph.py
+++ change_paragraph.py
@@ -22,6 +22,18 @@ class Word(object):
                     new[offset]=letter
                     inline[i].text=''.join(new)
                     position_at=position_at+1;
+                 
+                    begin_align=0
+                    for j in range(len(inline)):
+                        if begin_align<=position_at<begin_align+len(inline[j].text) :
+                            offset_align=position_at-begin_align
+                            if letter.isupper():
+                                
+                                inline[j].text=inline[j].text[:offset_align]+inline[j].text[offset_align+1:]
+                            break    
+                        begin_align=begin_align+len(inline[j].text)    
+                       
+
                     break 
                 begin=begin+len(inline[i].text)
         return paragraph
@@ -32,25 +44,39 @@ if __name__ == "__main__":
 
     doc = docx.Document('Field Problem Feedback-Template.docx')
 
-    for i in range(len(doc.paragraphs[0].runs)):
-        print(doc.paragraphs[0].runs[0].text)
-        print("size=",len(doc.paragraphs[0].runs[i].text))
+    for j in  doc.paragraphs:
+
+        num=0
+        for i in range(len(j.runs)):
+       
+            num=len(j.runs[i].text)+num
+            print(j.runs[i].text)
+            print("size=",len(j.runs[i].text))
+        
+        print("number=",num)
+        #break
 
     word=Word
 
-    data="Entry Date："
-    value="test"
+    data="Product line ："
+    value="SCHINDLER 3300 "
 
-    doc.paragraphs[0]=word.input_data(doc.paragraphs[0],data,value)
+    doc.paragraphs[0]=word.input_data(doc.paragraphs[1],data,value)
     doc.save('tmp.docx')
 
-    for i in range(len(doc.paragraphs[0].runs)):
-        print(doc.paragraphs[0].runs[0].text)
-        print("size=",len(doc.paragraphs[0].runs[i].text))
+    doc = docx.Document('tmp.docx')
 
+    for j in  doc.paragraphs:
 
-
-
+        num=0
+        for i in range(len(j.runs)):
+       
+            num=len(j.runs[i].text)+num
+            print(j.runs[i].text)
+            print("size=",len(j.runs[i].text))
+        
+        print("number=",num)
+        #break
 
 
 
