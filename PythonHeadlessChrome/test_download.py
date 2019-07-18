fa87b1981110a386aaff244edde6f6092fa453b0
Johnny SHI <johnny.shi@schindler.com>
2019-07-18 14:29:03 +0800

    delete useless files

--- PythonHeadlessChrome/test_download.py
+++ PythonHeadlessChrome/test_download.py
@@ -8,8 +8,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome import webdriver as chrome_webdriver
 from selenium.webdriver import Chrome
 
-class TestDownload:
-    def test_download(self):
+class RMPDownload:
+    def download(self,equipment):
 
         driver_builder = DriverBuilder()
         download_path = path.dirname(path.realpath(__file__))
@@ -28,11 +28,7 @@ class TestDownload:
 
         driver = driver_builder.get_driver(download_path, headless=True)
 
-        driver.get("http://rmp.global.schindler.com/Equipment/EquipmentMain/EquipmentDetails/?sapSys=ZAP&equnr=10000021") 
-
-        print("title of baidu=",driver.title)
- 
-        print("url =",driver.current_url)
+        driver.get("http://rmp.global.schindler.com/Equipment/EquipmentMain/EquipmentDetails/?sapSys=ZAP&equnr="+equipment) 
 
         try :
             js = ''' document.querySelectorAll("li")[36].click();
@@ -41,7 +37,7 @@ class TestDownload:
             '''
             driver.execute_script(js)
         except: 
-            print("exception")
+            print("exception 1 ")
 
         sleep(5)  
  
@@ -49,25 +45,72 @@ class TestDownload:
             js = ''' document.getElementById('ExportWithParameters1').click(); '''
             driver.execute_script(js)
         except:
-            print("exception")
+            print("exception 2 ")
 
-        self.wait_until_file_exists(expected_download, 600)
+        self.wait_until_file_exists(equipment+"_MessagesExport_", 20)
+
+        try :
+            js = ''' document.querySelectorAll("li")[35].click();
+
+ '''
+            driver.execute_script(js)
+        except: 
+            print("exception 3 ")
+
+        sleep(5)  
+ 
+        try :
+            js = ''' document.getElementById('EventExportButton').click(); '''
+            driver.execute_script(js)
+        except:
+            print("exception 4 ")
+
+        self.wait_until_file_exists(equipment+"_EventsExport_", 20)
         driver.close()
-
-        assert (path.isfile(expected_download))
 
         print("done")
 
     def wait_until_file_exists(self, actual_file, wait_time_in_seconds=5):
         waits = 0
-        while not path.isfile(actual_file) and waits < wait_time_in_seconds:
+        path="./"
+                      
+        while waits < wait_time_in_seconds:
+            fileList=os.listdir(path)
+            for file in fileList:
+               print("file name=",file)
+               if file.find(actual_file) >=0 :
+                    return 
+
             print("sleeping...." + str(waits))
             sleep(.5)  # make sure file completes downloading
             waits += .5
 
 
+
+
 if __name__ == "__main__":
  
     print("testing download")
-    t=TestDownload()
-    t.test_download()
+    path_root="./"
+
+    fileList=os.listdir(path_root)
+    for file in fileList:
+        print("file name=",file)
+        if file.find("10000021") >=0 :
+            os.remove(path+file)
+
+
+    t=RMPDownload()
+    t.download("10000021")
+
+
+
+
+
+
+
+
+
+
+
+
